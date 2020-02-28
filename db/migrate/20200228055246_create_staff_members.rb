89101258462662a576d90568ee0aa02167a26119
terufumi1122 <terufumi1122@gmail.com>
2020-02-28 15:08:46 +0900

    chore: db:migrate

--- db/migrate/20200228055246_create_staff_members.rb
+++ db/migrate/20200228055246_create_staff_members.rb
@@ -1,15 +1,15 @@
 class CreateStaffMembers < ActiveRecord::Migration[6.0]
   def change
     create_table :staff_members do |t|
-      t.string  :email,            null: false
-      t.string  :family_name,      null: false
-      t.string  :given_name,       null: false
-      t.string  :family_name_kana, null: false
-      t.string  :given_name_kana,  null: false
-      t.string  :hashed_password
-      t.date    :start_date,       null: false
-      t.date    :end_date
-      t.boolean :suspended,        null: false, default: false
+      t.string  :email,            null: false                  # メールアドレス
+      t.string  :family_name,      null: false                  # 姓
+      t.string  :given_name,       null: false                  # 名
+      t.string  :family_name_kana, null: false                  # 姓（カナ）
+      t.string  :given_name_kana,  null: false                  # 名（カナ）
+      t.string  :hashed_password                                # パスワード
+      t.date    :start_date,       null: false                  # 開始日
+      t.date    :end_date                                       # 終了日
+      t.boolean :suspended,        null: false, default: false  # 無効フラグ
 
       t.timestamps
     end
