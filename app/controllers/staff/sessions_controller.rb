9d6843bf33347bff75eb2595732df3414ab2e7c6
terufumi1122 <terufumi1122@gmail.com>
2020-02-28 19:39:34 +0900

    chore: add staff/sessions_controller.rb

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -1,2 +1,10 @@
-class Staff::SessionsController < ApplicationController
+class Staff::SessionsController < Staff::Base
+  def new
+    if current_staff_member
+      redirect_to :staff_root
+    else
+      @form = Staff::LoginForm.new
+      render action: "new"
+    end
+  end
 end
