c5747d76d8f9ded3920f442cb0f0f1f9fd69ff89
terufumi1122 <terufumi1122@gmail.com>
2020-02-28 14:29:21 +0900

    fix: indent

--- config/initializers/exceptions_app.rb
+++ config/initializers/exceptions_app.rb
@@ -3,11 +3,11 @@ Rails.application.configure do
     request = ActionDispatch::Request.new(env)
 
     action = 
-    case request.path_info
-    when "/404"; :not_found
-    when "/422"; :unprocessable_entity
-    else; :internal_server_error
-    end
+      case request.path_info
+      when "/404"; :not_found
+      when "/422"; :unprocessable_entity
+      else; :internal_server_error
+      end
 
     ErrorsController.action(action).call(env)
   end
