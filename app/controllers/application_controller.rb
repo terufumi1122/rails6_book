d98d4d9b5a563f945cf818f5f3cd8f0b09f11a06
terufumi1122 <terufumi1122@gmail.com>
2020-02-28 14:47:15 +0900

    add include ErrorHandlers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   class Forbidden         < ActionController::ActionControllerError; end
   class IpAddressRejected < ActionController::ActionControllerError; end
 
+  include ErrorHandlers
+
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
       Regexp.last_match[1]
