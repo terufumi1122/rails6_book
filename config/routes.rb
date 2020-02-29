82c403cc77edd1c5d70b2b995f5e13bcf3c5deb7
terufumi1122 <terufumi1122@gmail.com>
2020-02-29 16:29:29 +0900

    chore: change routes using config

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+  config = Rails.application.config.baukis2
 
-  constraints host: "baukis2.example.com" do
-    namespace :staff, path: "" do
+  constraints host: config[:staff][:host] do
+    namespace :staff, path: config[:staff][:path] do
       root "top#index"
       get "login"      => "sessions#new",    as: :login
       resource :session, only:   [ :create, :destroy ]
       resource :account, except: [ :new, :create, :destroy ]
     end
   end
-  
+
   namespace :admin do
     root "top#index"
     get "login"      => "sessions#new",    as: :login
